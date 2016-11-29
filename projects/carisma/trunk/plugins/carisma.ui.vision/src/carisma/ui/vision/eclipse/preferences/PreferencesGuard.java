7f35f2c7bf1a5db17727b5843dbbb5990ea19489
speldszus <speldszus@b99f87ec-93df-11e5-8f70-1b308f1eeebc>
2016-11-29 14:48:54 +0000

    Fixed classes wich have access to VisiOn db preferences
    git-svn-id: https://svn.uni-koblenz.de/rgse/carisma/trunk@9394 b99f87ec-93df-11e5-8f70-1b308f1eeebc

--- projects/carisma/trunk/plugins/carisma.ui.vision/src/carisma/ui/vision/eclipse/preferences/PreferencesGuard.java
+++ projects/carisma/trunk/plugins/carisma.ui.vision/src/carisma/ui/vision/eclipse/preferences/PreferencesGuard.java
@@ -2,11 +2,14 @@ package carisma.ui.vision.eclipse.preferences;
 
 import java.security.Guard;
 
-import carisma.ui.vision.eclipse.preferences.initializer.VisionInitializer;
+import carisma.ui.vision.popup.actions.VisiOnDBOutput;
+import carisma.ui.vision.questions.QuestionGenerationAction;
+import carisma.vision.dbAccess.dbAccess;
+
 
 public final class PreferencesGuard implements Guard {
 
-	private static final Class<?>[] allowedClasses = {VisionInitializer.class};
+	private static final Class<?>[] allowedClasses = {dbAccess.class, VisiOnDBOutput.class, QuestionGenerationAction.class};
 	
 	@Override
 	public void checkGuard(Object accessingObject) throws SecurityException {
