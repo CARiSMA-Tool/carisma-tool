558025f4e6fceb1efef9e70d9d05a78e7455a222
strueber <strueber@b99f87ec-93df-11e5-8f70-1b308f1eeebc>
2017-01-24 10:14:29 +0000

    Update Analyzer to report a more specific error message
    git-svn-id: https://svn.uni-koblenz.de/rgse/carisma/trunk@9423 b99f87ec-93df-11e5-8f70-1b308f1eeebc

--- projects/carisma/trunk/plugins/carisma.core/src/carisma/core/analysis/Analyzer.java
+++ projects/carisma/trunk/plugins/carisma.core/src/carisma/core/analysis/Analyzer.java
@@ -132,7 +132,7 @@ public class Analyzer implements AnalysisHost {
 			this.currentCheckResult.setName("ERROR");
 			this.result.addCheckResult(this.currentCheckResult);
 			addResultMessage(new AnalysisResultMessage(StatusType.ERROR,
-					"Model file not found. Please check your analysis configuration."));
+					"An error occured while loading the model: "+e.getMessage()));
 			this.result.setStatus(AnalysisResultStatus.FAIL);
 			this.uiConnector.updateView();
 			Logger.log(LogLevel.ERROR, "Unable to load model '" + file.getAbsolutePath() + "'", e);
