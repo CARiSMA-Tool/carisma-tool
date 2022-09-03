209bae5d096360a91f5e8b9ee8fc487c61e79597
alexpeikert <alex.peikert.ap@gmail.com>
2022-09-03 20:08:32 +0200

    added pom.xml

---
+++ plugins/carisma.profile.umlsec.umlsec4ids/model-gen/carisma/profile/umlsec/umlsec4ids/impl/Umlsec4idsPackageImpl.java
@@ -0,0 +1,213 @@
+/**
+ */
+package carisma.profile.umlsec.umlsec4ids.impl;
+
+import carisma.profile.umlsec.umlsec4ids.Umlsec4idsFactory;
+import carisma.profile.umlsec.umlsec4ids.Umlsec4idsPackage;
+import carisma.profile.umlsec.umlsec4ids.basefree;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EcorePackage;
+
+import org.eclipse.emf.ecore.impl.EPackageImpl;
+
+import org.eclipse.uml2.types.TypesPackage;
+
+import org.eclipse.uml2.uml.UMLPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Package</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class Umlsec4idsPackageImpl extends EPackageImpl implements Umlsec4idsPackage {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private EClass basefreeEClass = null;
+
+	/**
+	 * Creates an instance of the model <b>Package</b>, registered with
+	 * {@link org.eclipse.emf.ecore.EPackage.Registry EPackage.Registry} by the package
+	 * package URI value.
+	 * <p>Note: the correct way to create the package is via the static
+	 * factory method {@link #init init()}, which also performs
+	 * initialization of the package, or returns the registered package,
+	 * if one already exists.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see org.eclipse.emf.ecore.EPackage.Registry
+	 * @see carisma.profile.umlsec.umlsec4ids.Umlsec4idsPackage#eNS_URI
+	 * @see #init()
+	 * @generated
+	 */
+	private Umlsec4idsPackageImpl() {
+		super(eNS_URI, Umlsec4idsFactory.eINSTANCE);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static boolean isInited = false;
+
+	/**
+	 * Creates, registers, and initializes the <b>Package</b> for this model, and for any others upon which it depends.
+	 *
+	 * <p>This method is used to initialize {@link Umlsec4idsPackage#eINSTANCE} when that field is accessed.
+	 * Clients should not invoke it directly. Instead, they should simply access that field to obtain the package.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #eNS_URI
+	 * @see #createPackageContents()
+	 * @see #initializePackageContents()
+	 * @generated
+	 */
+	public static Umlsec4idsPackage init() {
+		if (isInited) return (Umlsec4idsPackage)EPackage.Registry.INSTANCE.getEPackage(Umlsec4idsPackage.eNS_URI);
+
+		// Obtain or create and register package
+		Object registeredUmlsec4idsPackage = EPackage.Registry.INSTANCE.get(eNS_URI);
+		Umlsec4idsPackageImpl theUmlsec4idsPackage = registeredUmlsec4idsPackage instanceof Umlsec4idsPackageImpl ? (Umlsec4idsPackageImpl)registeredUmlsec4idsPackage : new Umlsec4idsPackageImpl();
+
+		isInited = true;
+
+		// Initialize simple dependencies
+		EcorePackage.eINSTANCE.eClass();
+		TypesPackage.eINSTANCE.eClass();
+		UMLPackage.eINSTANCE.eClass();
+
+		// Create package meta-data objects
+		theUmlsec4idsPackage.createPackageContents();
+
+		// Initialize created meta-data
+		theUmlsec4idsPackage.initializePackageContents();
+
+		// Mark meta-data to indicate it can't be changed
+		theUmlsec4idsPackage.freeze();
+
+		// Update the registry and return the package
+		EPackage.Registry.INSTANCE.put(Umlsec4idsPackage.eNS_URI, theUmlsec4idsPackage);
+		return theUmlsec4idsPackage;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EClass getbasefree() {
+		return basefreeEClass;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EReference getbasefree_Base_Node() {
+		return (EReference)basefreeEClass.getEStructuralFeatures().get(0);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Umlsec4idsFactory getUmlsec4idsFactory() {
+		return (Umlsec4idsFactory)getEFactoryInstance();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private boolean isCreated = false;
+
+	/**
+	 * Creates the meta-model objects for the package.  This method is
+	 * guarded to have no affect on any invocation but its first.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void createPackageContents() {
+		if (isCreated) return;
+		isCreated = true;
+
+		// Create classes and their features
+		basefreeEClass = createEClass(BASEFREE);
+		createEReference(basefreeEClass, BASEFREE__BASE_NODE);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private boolean isInitialized = false;
+
+	/**
+	 * Complete the initialization of the package and its meta-model.  This
+	 * method is guarded to have no affect on any invocation but its first.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void initializePackageContents() {
+		if (isInitialized) return;
+		isInitialized = true;
+
+		// Initialize package
+		setName(eNAME);
+		setNsPrefix(eNS_PREFIX);
+		setNsURI(eNS_URI);
+
+		// Obtain other dependent packages
+		UMLPackage theUMLPackage = (UMLPackage)EPackage.Registry.INSTANCE.getEPackage(UMLPackage.eNS_URI);
+
+		// Create type parameters
+
+		// Set bounds for type parameters
+
+		// Add supertypes to classes
+
+		// Initialize classes, features, and operations; add parameters
+		initEClass(basefreeEClass, basefree.class, "basefree", !IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+		initEReference(getbasefree_Base_Node(), theUMLPackage.getNode(), null, "base_Node", null, 1, 1, basefree.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_COMPOSITE, IS_RESOLVE_PROXIES, !IS_UNSETTABLE, IS_UNIQUE, !IS_DERIVED, !IS_ORDERED);
+
+		// Create resource
+		createResource(eNS_URI);
+
+		// Create annotations
+		// http://www.eclipse.org/uml2/2.0.0/UML
+		createUMLAnnotations();
+	}
+
+	/**
+	 * Initializes the annotations for <b>http://www.eclipse.org/uml2/2.0.0/UML</b>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void createUMLAnnotations() {
+		String source = "http://www.eclipse.org/uml2/2.0.0/UML";
+		addAnnotation
+		  (this,
+		   source,
+		   new String[] {
+			   "originalName", "umlsec4ids"
+		   });
+	}
+
+} //Umlsec4idsPackageImpl
