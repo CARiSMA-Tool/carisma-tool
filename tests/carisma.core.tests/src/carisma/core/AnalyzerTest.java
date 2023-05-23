a37c7e23af769a2a87b1f624dbbbc9428b49cbf1
Alexander Peikert <alex.peikert.ap@gmail.com>
2023-05-23 22:14:09 +0200

    AnalyzerTest set to Ignore, since it is not implemented yet.

--- tests/carisma.core.tests/src/carisma/core/AnalyzerTest.java
+++ tests/carisma.core.tests/src/carisma/core/AnalyzerTest.java
@@ -2,12 +2,14 @@ package carisma.core;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AnalyzerTest {
 
 	@SuppressWarnings("static-method")
 	@Test
+	@Ignore("Test not yet implemented")
 	public void test() {
 		fail("Not yet implemented");
 	}
