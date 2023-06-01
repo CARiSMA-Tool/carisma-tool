47eadd4109d35840d4b0959735e62c098a1ff1f3
Julian Flake <flake@uni-koblenz.de>
2023-06-01 11:02:52 +0000

    More Integer and Float constructor replaced by factory methods
    Addresses #20

--- plugins/carisma.check.template/templates_3.4/preferencepagetemplate/java/AccessPreferences.java
+++ plugins/carisma.check.template/templates_3.4/preferencepagetemplate/java/AccessPreferences.java
@@ -30,7 +30,7 @@ public class AccessPreferences  {
 
 			IPreferenceStore store = $activator$.getDefault().getPreferenceStore();
 			String[] pref = store.getString(PreferenceConstants.COLOR).split(",");
-			return new Color(new Integer(pref[0]), new Integer(pref[1]), new Integer(pref[2]));
+			return new Color(Integer.parseInt(pref[0]), Integer.parseInt(pref[1]), Integer.parseInt(pref[2]));
 		}
 		
 		
