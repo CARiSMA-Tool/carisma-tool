e3d6eba9aca422cd58dfaf21afa1288864627b51
Nils Rodewald <nrodewald@uni-koblenz.de>
2024-07-07 19:18:02 +0200

    created test-project for the uconpolicycreation-plugin

---
+++ tests/carisma.check.uconpolicycreation.tests/src/carisma/check/uconpolicycreation/tests/InternalModelTests.java
@@ -0,0 +1,16 @@
+package carisma.check.uconpolicycreation.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class InternalModelTests {
+
+	
+	
+	@Test
+	public void emptyTest() {
+		System.out.println("In empty test");
+		assertTrue(true);
+	}
+}
