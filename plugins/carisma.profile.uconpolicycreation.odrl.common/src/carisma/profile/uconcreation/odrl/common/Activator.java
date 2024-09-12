971981e7d4984604a1a068a4cf40a06ad7bf39a2
Nils Rodewald <nrodewald@uni-koblenz.de>
2024-09-12 16:38:56 +0200

    include changes that fix the activator not being found in maven testing

--- plugins/carisma.profile.uconpolicycreation.odrl.common/src/carisma/profile/uconcreation/odrl/common/Activator.java
+++ plugins/carisma.profile.uconpolicycreation.odrl.common/src/carisma/profile/uconcreation/odrl/common/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "carisma.profile.uconcreation.common"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "carisma.profile.uconpolicycreation.odrl.common"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
